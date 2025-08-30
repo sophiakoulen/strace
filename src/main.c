501de238a107c30ba3d53b8ba9c0cdd0fdd368ed
Sophia Koulen <sophia.koulen@outlook.com>
2025-08-30 17:51:29 +0200

    found syscall number in orig_rax.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,31 @@
 #include <sys/uio.h>
 #include <sys/user.h>
 
+void wait_and_print(int pid)
+{
+	int status;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+	{
+		printf("child exited with code %d\n", WEXITSTATUS(status));
+		exit(0);
+	}
+	else if (WIFSIGNALED(status))
+	{
+		printf("child got signal no %d\n", WTERMSIG(status));
+	   	exit(0);	
+	}
+	else if (WIFSTOPPED(status))
+	{
+		printf("child stopped with signal no %d\n", WSTOPSIG(status));
+	}
+	else
+	{
+		printf("something else happened.\n");
+	}
+	fflush(stdout);
+}
+
 void stop_at_syscall(int pid)
 {
 	if (ptrace(PTRACE_SYSCALL, pid, (void*)0, (void*)0) < 0)
@@ -13,10 +38,10 @@ void stop_at_syscall(int pid)
 		perror("problem with ptrace SYSCALL");
 		exit(4);
 	}
-	waitpid(pid, NULL, 0);
+	wait_and_print(pid);
 }
 
-void print_registers(int pid)
+void print_sys_enter(int pid)
 {
 	struct iovec data;
 	data.iov_base = alloca(1024);
@@ -29,7 +54,24 @@ void print_registers(int pid)
 	}
 
 	struct user_regs_struct *regs = data.iov_base;
-	printf("rax = %llu\n", regs->rax);
+	printf("syscall number %lld with args: rdi = %llu, rsi = %llu, rdx = %llu, rcx = %llu\n",
+			regs->orig_rax, regs->rdi, regs->rsi, regs->rdx, regs->rcx);
+}
+
+void print_sys_exit(int pid)
+{
+	struct iovec data;
+	data.iov_base = alloca(1024);
+	data.iov_len = 1024;
+
+	if (ptrace(PTRACE_GETREGSET, pid, 1, &data) < 0)
+	{
+		perror("Problem with ptrace GETREGSET");
+		exit(3);
+	}
+
+	struct user_regs_struct *regs = data.iov_base;
+	printf("return value = %lld\n", regs->rax);
 }
 
 int main(int argc, char** argv)
@@ -49,37 +91,35 @@ int main(int argc, char** argv)
 	else if (pid == 0)
 	{
 		//in child process;
-		printf("child\n");
+		printf("Hi i'm the child process\n");
+		fflush(stdout);
 
-		//wait for parent
-		sleep(1);
+		//send stop to itself
+		kill(getpid(), SIGSTOP);
 
-		//need to execve
 		if (execvp(argv[1], argv+1) < 0)
 			perror("strace: problem with execvp");
 	}
 	else
 	{
 		//in parent process;
-		printf("parent proc of pid %d\n", pid);
+		printf("hello i'm the parent of pid %d\n", pid);
+		fflush(stdout);
 
 		if (ptrace(PTRACE_SEIZE, pid, (void*)0, (void*)0) < 0)
-			perror("problem with ptrace");
+			perror("problem with ptrace A");
 
+		//if (ptrace(PTRACE_INTERRUPT, pid, (void*)0, (void*)0) < 0)
+		//	perror("problem with ptrace B");
 
-		if (ptrace(PTRACE_INTERRUPT, pid, (void*)0, (void*)0) < 0)
-			perror("problem with ptrace");
-
-		waitpid(pid, NULL, 0);
+		wait_and_print(pid);
 
 		while (1)
 		{
 			stop_at_syscall(pid);
-			print_registers(pid);
+			print_sys_enter(pid);
 			stop_at_syscall(pid);
-			print_registers(pid);
+			print_sys_exit(pid);
 		}
-
-		waitpid(pid, NULL, 0);
 	}
 }
