0ccc3513e81e93d36b688560b1a1210adf44912c
Sophia Koulen <sophia.koulen@outlook.com>
2025-09-07 21:50:35 +0200

    print to stderr.

--- src/main.c
+++ src/main.c
@@ -41,147 +41,142 @@ struct i386_user_regs_struct {
 int current_status = OUT_SYS;
 int current_mode = X86_64;
 
-int wait_and_print(int pid)
+void wait_for_syscall(int pid)
 {
 	int status;
 	waitpid(pid, &status, __WALL);
 	if (WIFEXITED(status))
 	{
 		if (current_status == IN_SYS)
-			printf("\n");
-
-		printf("+++ exited with %d +++\n", WEXITSTATUS(status));
-		fflush(stdout);
+			fprintf(stderr,"\n");
+		fprintf(stderr,"+++ exited with %d +++\n", WEXITSTATUS(status));
 		exit(0);
 	}
 	else if (WIFSIGNALED(status))
 	{
 		if (current_status == IN_SYS)
-			printf("\n");
-
-		printf("+++ killed by %s +++\n", sigabbrev[WTERMSIG(status)]);
+			fprintf(stderr,"\n");
+		fprintf(stderr,"+++ killed by %s +++\n", sigabbrev[WTERMSIG(status)]);
 		fflush(stdout);
 	   	exit(0);	
 	}
 	else if (WIFSTOPPED(status))
 	{
 		if (WSTOPSIG(status) == (SIGTRAP|0x80))
+			return;
+
+		if ((WSTOPSIG(status) == SIGSTOP
+			|| WSTOPSIG(status) == SIGTSTP
+			|| WSTOPSIG(status) == SIGTTIN
+			|| WSTOPSIG(status) == SIGTTOU)
+			&& status>>16  ==  PTRACE_EVENT_STOP)
 		{
-			//entry or exit from syscall
-			return 0;
+				if (current_status == IN_SYS)
+					fprintf(stderr,"\n");
+				fprintf(stderr,"-- stopped by %s ---\n", sigabbrev[WSTOPSIG(status)]);
+				fflush(stderr);
+
+				if (ptrace(PTRACE_LISTEN, pid, 0, WSTOPSIG(status)))
+				{
+					perror("strace: ptrace");
+					exit(1);
+				}
+				return wait_for_syscall(pid);
 		}
 		else
 		{
-			if ((WSTOPSIG(status) == SIGSTOP
-				|| WSTOPSIG(status) == SIGTSTP
-				|| WSTOPSIG(status) == SIGTTIN
-				|| WSTOPSIG(status) == SIGTTOU)
-				&& status>>16  ==  PTRACE_EVENT_STOP)
+			if (current_status == IN_SYS)
+				fprintf(stderr,"\n");
+			fprintf(stderr,"--- %s ---\n", sigabbrev[WSTOPSIG(status)]);
+			fflush(stderr);
+
+			if (ptrace(PTRACE_SYSCALL, pid, 0, WSTOPSIG(status)))
 			{
-					if (current_status == IN_SYS)
-						printf("\n");
-
-					printf("-- stopped by %s ---\n", sigabbrev[WSTOPSIG(status)]);
-					fflush(stdout);
-
-					if (ptrace(PTRACE_LISTEN, pid, 0, WSTOPSIG(status)))
-					{
-						perror("problem with PTRACE_LISTEN");
-						exit(1);
-					}
-					return wait_and_print(pid);
+				perror("strace: ptrace");
+				exit(1);
 			}
-			else
-			{
-				if (current_status == IN_SYS)
-					printf("\n");
-
-				//need to print additional siginfo
-				printf("--- %s ---\n", sigabbrev[WSTOPSIG(status)]);
-				fflush(stdout);
-
-				if (ptrace(PTRACE_SYSCALL, pid, 0, WSTOPSIG(status)))
-				{
-					perror("problem with PTRACE_SYSCALL");
-					exit(1);
-				}
-
-				return wait_and_print(pid);
-			}
+			return wait_for_syscall(pid);
 		}
 	}
-
 	else
 	{
-		printf("HELP SOMETHING ELSE HAPPENED.\n");
-		fflush(stdout);
+		fprintf(stderr, "strace: waitpid returned an unexpected status.\n");
 		exit(1);
 	}
-	return 0;
 }
 
 void stop_at_syscall(int pid)
 {
 	if (ptrace(PTRACE_SYSCALL, pid, (void*)0, (void*)0) < 0)
 	{
-		perror("problem with ptrace SYSCALL");
-		exit(4);
+		perror("strace: ptrace");
+		exit(1);
 	}
 }
 
-void print_arg32(uint32_t reg, enum arg_type_e type)
+#define GETMEM_BUFFERSIZE 1024
+ssize_t getmem(int pid, char *buffer, void* addr)
+{
+	struct iovec  local[1];
+	struct iovec  remote[1];
+
+	local[0].iov_base = buffer;
+	local[0].iov_len = GETMEM_BUFFERSIZE;
+	remote[0].iov_base = addr;
+	remote[0].iov_len = GETMEM_BUFFERSIZE;
+
+	ssize_t ret = process_vm_readv(pid, local, 1, remote, 1, 0);
+	buffer[GETMEM_BUFFERSIZE - 1] = '\0';
+	return ret;
+}
+
+void print_arg32(int pid, uint32_t reg, enum arg_type_e type)
 {
 	 if (type == INT)
-		 printf("%d", reg);
+		 fprintf(stderr,"%d", reg);
 	 else if (type == UINT)
-		 printf("%u", reg);
+		 fprintf(stderr,"%u", reg);
 	 else if (type == STR)
-		 printf("\"%s\"", (char*)reg);
+	 {
+		 char buffer[1024];
+		 if (getmem(pid, buffer, (void*)reg) == -1)
+		 	fprintf(stderr,"%p", (void*)reg);
+		 else
+			fprintf(stderr,"\"%s\"", buffer);
+	 }
 	 else
-		 printf("%p", (void*)reg);
+		 fprintf(stderr,"%p", (void*)reg);
 }
 
 void print_arg64(int pid, unsigned long long int reg, enum arg_type_e type)
 {
 	 if (type == INT)
-		 printf("%lld", reg);
+		 fprintf(stderr,"%lld", reg);
 	 else if (type == UINT)
-		 printf("%llu", reg);
+		 fprintf(stderr,"%llu", reg);
 	 else if (type == STR)
 	 {
-		 char          buf1[1024];
-         struct iovec  local[1];
-         struct iovec  remote[1];
-
-           local[0].iov_base = buf1;
-           local[0].iov_len = 1024;
-           
-		   remote[0].iov_base = (void *)reg;
-           remote[0].iov_len = 1024;
-
-           ssize_t nread = process_vm_readv(pid, local, 1, remote, 1, 0);
-           if (nread == -1)
-		   {
-               perror("problem with process_vm_readv");
-			   exit(-1);
-		   }
-		   else
-		printf("\"%s\"", (char*)(local[0].iov_base));
+		 char buffer[1024];
+		 if (getmem(pid, buffer, (void*)reg) == -1)
+		 	fprintf(stderr,"%p", (void*)reg);
+		 else
+			fprintf(stderr,"\"%s\"", buffer);
 	 }
 	 else
-		 printf("%p", (void*)reg);
+		 fprintf(stderr,"%p", (void*)reg);
 }
 
 void print_sys_enter(int pid)
 {
 	struct iovec data;
-	data.iov_base = alloca(1024);
-	data.iov_len = 1024;
+	char buf[1024];
+	data.iov_base = buf;
+	data.iov_len = sizeof(buf);
 
 	if (ptrace(PTRACE_GETREGSET, pid, 1, &data) < 0)
 	{
-		perror("Problem with ptrace GETREGSET");
-		exit(3);
+		perror("strace: ptrace:");
+		exit(1);
 	}
 
 	if (data.iov_len == sizeof(struct user_regs_struct))
@@ -189,47 +184,51 @@ void print_sys_enter(int pid)
 		if (current_mode != X86_64)
 		{
 			current_mode = X86_64;
-			printf("[ Process PID=%d runs in 64 bit mode. ]\n", pid);
+			fprintf(stderr,"[ Process PID=%d runs in 64 bit mode. ]\n", pid);
 		}
+
 		struct user_regs_struct *regs = data.iov_base;
-		printf("%s(", syscalls[regs->orig_rax].name);
+		fprintf(stderr,"%s(", syscalls[regs->orig_rax].name);
 		unsigned int l = syscalls[regs->orig_rax].arg_count;
 		unsigned long long int args[6] = {regs->rdi, regs->rsi, regs->rdx, regs->r10, regs->r8, regs->r9};
 		unsigned int i;
-		for (i = 0; i + 1 < l; i++)
+		for (i = 0; i < l; i++)
 		{
 			print_arg64(pid, args[i], (syscalls[regs->orig_rax].args)[i]);
-			printf(", ");
+			if (i + 1 < l)
+				fprintf(stderr,", ");
+			else
+				fprintf(stderr,")");
 		}
-		if (l > 0)
-			print_arg64(pid, args[i], syscalls[regs->orig_rax].args[i]);
-		printf(")");
+		fflush(stderr);
 	}
 	else if (data.iov_len == sizeof(struct i386_user_regs_struct))
 	{
 		if (current_mode != X86_32)
 		{
 			current_mode = X86_32;
-			printf("[ Process PID=%d runs in 32 bit mode. ]\n", pid);
+			fprintf(stderr,"[ Process PID=%d runs in 32 bit mode. ]\n", pid);
 		}
+
 		struct i386_user_regs_struct *regs = data.iov_base;
-		printf("%s(", syscalls32[regs->orig_eax].name);
+		fprintf(stderr,"%s(", syscalls32[regs->orig_eax].name);
 		unsigned int l = syscalls32[regs->orig_eax].arg_count;
 		unsigned long long int args[6] = {regs->ebx, regs->ecx, regs->edx, regs->esi, regs->edi, regs->ebp};
 		unsigned int i;
-		for (i = 0; i + 1 < l; i++)
+		for (i = 0; i < l; i++)
 		{
-			print_arg32(args[i], (syscalls32[regs->orig_eax].args)[i]);
-			printf(", ");
+			print_arg32(pid, args[i], (syscalls32[regs->orig_eax].args)[i]);
+			if (i + 1 < l)
+				fprintf(stderr, ", ");
+			else
+				fprintf(stderr, ")");
 		}
-		if (l > 0)
-			print_arg32(args[i], syscalls32[regs->orig_eax].args[i]);
-		printf(")");
+		fflush(stderr);
 	}
 	else
 	{
-		printf("PROBLEM\n");
-		exit(3);
+		fprintf(stderr,"strace: ptrace returned an expected result.\n");
+		exit(1);
 	}
 	current_status = IN_SYS;
 }
@@ -237,42 +236,76 @@ void print_sys_enter(int pid)
 void print_sys_exit(int pid)
 {
 	struct iovec data;
-	data.iov_base = alloca(1024);
-	data.iov_len = 1024;
+	char buffer[1024];
+	data.iov_base = buffer;
+	data.iov_len = sizeof(buffer);
 
 	if (ptrace(PTRACE_GETREGSET, pid, 1, &data) < 0)
 	{
-		perror("Problem with ptrace GETREGSET");
-		exit(3);
+		perror("strace: ptrace");
+		exit(1);
 	}
 
 	if (data.iov_len == sizeof(struct user_regs_struct))
 	{
+		struct user_regs_struct *regs = data.iov_base;
+		if ((long long int)regs->rax >= -4095 && (long long int)regs->rax <= -1)
+			fprintf(stderr, "\t= %s\n", strerrorname_np(-1 * regs->rax));
+		else
+			fprintf(stderr, "\t= %lld\n", regs->rax);
+
 		if (current_mode != X86_64)
 		{
 			current_mode = X86_64;
-			printf("[ Process PID=%d runs in 64 bit mode. ]\n", pid);
+			fprintf(stderr, "[ Process PID=%d runs in 64 bit mode. ]\n", pid);
 		}
-		struct user_regs_struct *regs = data.iov_base;
-		if ((long long int)regs->rax >= -4095 && (long long int)regs->rax <= -1)
-			printf("\t= %s\n", strerrorname_np(-1 * regs->rax));
-		else
-			printf("\t= %lld\n", regs->rax);
 	}
 	else
 	{
+		struct i386_user_regs_struct *regs = data.iov_base;
+		if ((int32_t)regs->eax >= -4095 && (int32_t)regs->eax <= -1)
+			fprintf(stderr,"\t= %s\n", strerrorname_np(-1 * regs->eax));
+		else
+			fprintf(stderr,"\t= %d\n", regs->eax);
+
 		if (current_mode != X86_32)
 		{
 			current_mode = X86_32;
-			printf("[ Process PID=%d runs in 32 bit mode. ]\n", pid);
+			fprintf(stderr,"[ Process PID=%d runs in 32 bit mode. ]\n", pid);
 		}
-		struct i386_user_regs_struct *regs = data.iov_base;
-		if ((int32_t)regs->eax >= -4095 && (int32_t)regs->eax <= -1)
-			printf("\t= %s\n", strerrorname_np(-1 * regs->eax));
-		else
-			printf("\t= %d\n", regs->eax);
 	}
 	current_status = OUT_SYS;
+}
+
+void trace(int pid)
+{
+	if (ptrace(PTRACE_SEIZE, pid, (void*)0, (void*)0) < 0)
+	{
+		perror("strace: ptrace");
+		exit(1);
+	}
+
+	if (waitpid(pid, NULL, __WALL) < 0)
+	{
+		perror("strace: waitpid");
+		exit(1);
+	}
+
+	if (ptrace(PTRACE_SETOPTIONS, pid, (void*)0, PTRACE_O_TRACESYSGOOD) < 0)
+	{
+		perror("strace: ptrace");
+		exit(1);
+	}
+
+	while (1)
+	{
+		stop_at_syscall(pid);
+		wait_for_syscall(pid);
+		print_sys_enter(pid);
+		stop_at_syscall(pid);
+		wait_for_syscall(pid);
+		print_sys_exit(pid);
+	}
 }
 
 int main(int argc, char** argv)
@@ -286,42 +319,17 @@ int main(int argc, char** argv)
 	int pid = fork();
 	if (pid < 0)
 	{
-		perror("strace: problem with fork");
-		exit(2);
+		perror("strace: fork");
+		exit(1);
 	}
 	else if (pid == 0)
 	{
-		//in child process;
-		//send stop to itself
 		kill(getpid(), SIGSTOP);
 
 		if (execvp(argv[1], argv+1) < 0)
-			perror("strace: problem with execvp");
+			perror("strace: execve");
+		exit(1);
 	}
 	else
-	{
-		//in parent process;
-		if (ptrace(PTRACE_SEIZE, pid, (void*)0, (void*)0) < 0)
-			perror("problem with ptrace SEIZE.\n");
-
-		waitpid(pid, NULL, 0);
-
-		if (ptrace(PTRACE_SETOPTIONS, pid, (void*)0, PTRACE_O_TRACESYSGOOD) < 0)
-			perror("problem with ptrace SETOPTIONS.\n");
-
-		while (1)
-		{
-			stop_at_syscall(pid);
-			
-			wait_and_print(pid);
-
-			print_sys_enter(pid);
-			
-			stop_at_syscall(pid);
-			
-			wait_and_print(pid);
-
-			print_sys_exit(pid);
-		}
-	}
+		trace(pid);
 }
